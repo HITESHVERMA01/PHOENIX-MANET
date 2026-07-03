666ef303189d3df82685f6a9af963b1513636b4b
Hitesh Verma <vermahitesh419@gmail.com>
2026-07-03 22:24:57 +0530

    Define structures for node state and routing info

--- src/phoenix/phoenix-types.h
+++ src/phoenix/phoenix-types.h
@@ -1,1 +1,77 @@
+#ifndef PHOENIX_TYPES_H
+#define PHOENIX_TYPES_H
 
+#include <vector>
+
+namespace ns3
+{
+
+struct NodeState
+{
+    uint32_t nodeId;
+
+    double x;
+
+    double y;
+
+    double speed;
+
+    double direction;
+
+    double residualEnergy;
+
+    double queueLength;
+
+    double rssi;
+
+    double collisionRate;
+
+    double hopCount;
+
+    double packetSuccessRate;
+};
+
+struct Prediction
+{
+    double futureRSSI;
+
+    double linkExpirationTime;
+
+    double futureDistance;
+
+    double futureEnergy;
+};
+
+struct PriorityVector
+{
+    double energy;
+
+    double stability;
+
+    double throughput;
+
+    double delay;
+
+    double reliability;
+
+    double lifetime;
+};
+
+struct RouteInfo
+{
+    std::vector<uint32_t> path;
+
+    double fitness;
+
+    double confidence;
+
+    double delay;
+
+    double energy;
+
+    double overhead;
+};
+
+}
+
+#endif
