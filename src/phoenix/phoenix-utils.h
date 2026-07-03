deb4c48b941e6bf252c2118d2ac3eb022a6e4a19
Hitesh Verma <vermahitesh419@gmail.com>
2026-07-03 22:23:26 +0530

    Implement PhoenixUtils class with utility functions
    Added utility functions for clamping, distance calculation, and normalization.

--- src/phoenix/phoenix-utils.h
+++ src/phoenix/phoenix-utils.h
@@ -1,1 +1,41 @@
+#ifndef PHOENIX_UTILS_H
+#define PHOENIX_UTILS_H
 
+#include <cmath>
+
+namespace ns3
+{
+
+class PhoenixUtils
+{
+public:
+
+    static double Clamp(double value,double low,double high)
+    {
+        if(value<low)
+            return low;
+
+        if(value>high)
+            return high;
+
+        return value;
+    }
+
+    static double Distance(double x1,double y1,double x2,double y2)
+    {
+        return std::sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+    }
+
+    static double Normalize(double value,double min,double max)
+    {
+        if(max==min)
+            return 0.0;
+
+        return (value-min)/(max-min);
+    }
+
+};
+
+}
+
+#endif
